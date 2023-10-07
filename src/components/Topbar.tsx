e2fe025d2c44bebb0cd3aaa2d1509f2273ee5e4d
Lykcord <qbaxCW@gmx.de>
2023-10-08 00:31:41 +0200

    topbar-data added

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,13 +1,21 @@
 import "./topbar.css"
+import logo from "../assets/default.png"
 
 export default function Topbar(props: {siteTitle: string}) {
 
     return (
         <>
             <div className={"topbar"}>
+            <div className={"topbar-title"}>
                 <h1>{props.siteTitle}</h1>
                 <a>placeholder</a>
             </div>
+            <div className={"topbar-data flex flex-row"}>
+                <a>6920 </a><a>Credits</a>
+                <img src={logo} alt={"defaultavatar"} id={"defaultavatar"}  draggable={false}/>
+            </div>
+            </div>
         </>
+
     )
 }
